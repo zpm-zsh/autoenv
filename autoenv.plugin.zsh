ab34d69e8615efccff09f0b3e3a68b0b4429d443
Grigorii Horos <horosgrisa@gmail.com>
2015-03-06 15:03:52 +0200

    modified:   autoenv.plugin.zsh

--- autoenv.plugin.zsh
+++ autoenv.plugin.zsh
@@ -10,75 +10,75 @@ if [[ -z $AUTOENV_COLORED ]]; then
 fi
 
 add_auth_file(){
-  if which shasum &> /dev/null
-      then hash=$(shasum "$1" | cut -d' ' -f 1)
-  else hash=$(sha1sum "$1" | cut -d' ' -f 1)
-  fi
-  echo "$1:$hash" >> $AUTOENV_AUTH_FILE
+    if which shasum &> /dev/null
+    then hash=$(shasum "$1" | cut -d' ' -f 1)
+    else hash=$(sha1sum "$1" | cut -d' ' -f 1)
+    fi
+    echo "$1:$hash" >> $AUTOENV_AUTH_FILE
 }
 
 check_and_run(){
-      if [[ $COLORS == true ]]
-      then
-      echo -e "$fg_no_bold[green]> $fg_no_bold[red]WARNING$reset_color"
-      echo -e "$fg_no_bold[green]> $fg_no_bold[blue]This is the first time you are about to source $fg_no_bold[yellow]\"$fg_bold[red]$1$fg_no_bold[yellow]\"$reset_color"
-      echo
-      echo -e "$fg_no_bold[green]----------------$reset_color"
-      if hash pygmentize 2>/dev/null
-        then
+    if [[ $COLORS == true ]]
+    then
+        echo -e "$fg_no_bold[green]> $fg_no_bold[red]WARNING$reset_color"
+        echo -e "$fg_no_bold[green]> $fg_no_bold[blue]This is the first time you are about to source $fg_no_bold[yellow]\"$fg_bold[red]$1$fg_no_bold[yellow]\"$reset_color"
         echo
-        cp $1 /tmp/.autoenv.sh
-        pygmentize -f 256 -g /tmp/.autoenv.sh
+        echo -e "$fg_no_bold[green]----------------$reset_color"
+        if hash pygmentize 2>/dev/null
+        then
+            echo
+            cp $1 /tmp/.autoenv.sh
+            pygmentize -f 256 -g /tmp/.autoenv.sh
+        else
+            echo -e "$fg_no_bold[green]"
+            cat $1
+        fi
+        echo
+        echo -e "$fg_no_bold[green]----------------$reset_color"
+        echo
+        echo -ne "$fg_no_bold[blue]Are you sure you want to allow this? $fg[cyan]($fg_no_bold[green]y$fg[cyan]/$fg_no_bold[red]N$fg[cyan]) $reset_color"
     else
-      echo -e "$fg_no_bold[green]"
-      cat $1
-  fi
-  echo
-  echo -e "$fg_no_bold[green]----------------$reset_color"
-  echo
-  echo -ne "$fg_no_bold[blue]Are you sure you want to allow this? $fg[cyan]($fg_no_bold[green]y$fg[cyan]/$fg_no_bold[red]N$fg[cyan]) $reset_color"
-else
-    echo "> WARNING"
-    echo "> This is the first time you are about to source \"$1\""
-    echo
-    echo "----------------"
-    echo
-    cat $1
-    echo
-    echo "----------------"
-    echo
-    echo -n "Are you sure you want to allow this? (y/N)"
-fi
-read answer
-if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]
-  then
-  add_auth_file $1
-  source $1
-fi
+        echo "> WARNING"
+        echo "> This is the first time you are about to source \"$1\""
+        echo
+        echo "----------------"
+        echo
+        cat $1
+        echo
+        echo "----------------"
+        echo
+        echo -n "Are you sure you want to allow this? (y/N)"
+    fi
+    read answer
+    if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]
+    then
+        add_auth_file $1
+        source $1
+    fi
 }
 
 check_and_exec(){
-  if which shasum &> /dev/null
-      then hash=$(shasum "$1" | cut -d' ' -f 1)
-  else hash=$(sha1sum "$1" | cut -d' ' -f 1)
-  fi
-  if grep "$1:$hash" "$AUTOENV_AUTH_FILE" >/dev/null 2>/dev/null
-      then
-      source $1
-  else
-    check_and_run $1
-fi
+    if which shasum &> /dev/null
+    then hash=$(shasum "$1" | cut -d' ' -f 1)
+    else hash=$(sha1sum "$1" | cut -d' ' -f 1)
+    fi
+    if grep "$1:$hash" "$AUTOENV_AUTH_FILE" >/dev/null 2>/dev/null
+    then
+        source $1
+    else
+        check_and_run $1
+    fi
 }
 
 autoenv_init(){
     _OP=$OLDPWD
     _P=`pwd`
-
+    
     if [[ -f $_OP/.out ]]
     then
         check_and_exec $_OP/.out
     fi
-
+    
     while [[ ! $_P == $_OP/* ]]
     do
         _OP=`dirname $_OP`
@@ -86,9 +86,9 @@ autoenv_init(){
         then
             check_and_exec $_OP/.out
         fi
-    done 
-
-
+    done
+    
+    
     while [[ ! $_OP == $_P  ]]
     do
         _P=`dirname $_P`
@@ -96,13 +96,13 @@ autoenv_init(){
         then
             check_and_exec $_P/.env
         fi
-    done 
-
-   if [[ -f $_P/.env ]]
+    done
+    
+    if [[ -f $_P/.env ]]
     then
         check_and_exec $_P/.env
     fi
-
+    
 }
 
 
