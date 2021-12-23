0dc14a4951990e8aef588e335aac43cca6708ea7
Grigorii Horos <horosgrisa@gmail.com>
2021-12-23 15:44:47 +0200

    Change url

--- autoenv.plugin.zsh
+++ autoenv.plugin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Standarized $0 handling, following:
-# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+# https://z-shell.github.io/zsh-plugin-assessor/Zsh-Plugin-Standard
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
