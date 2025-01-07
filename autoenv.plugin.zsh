4d237a96f5c0ca7eec2628805685cc285430fc64
Grigorii Horos <horosgrisa@gmail.com>
2025-01-08 01:40:10 +0200

    Change location

--- autoenv.plugin.zsh
+++ autoenv.plugin.zsh
@@ -11,12 +11,15 @@ if [[ $PMSPEC != *f* ]] {
 
 autoload -Uz autoenv_chdir autoenv_check_and_run autoenv_check_and_exec
 
-: ${AUTOENV_AUTH_FILE:="$HOME/.autoenv_authorized"}
+: ${AUTOENV_AUTH_FILE:="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/autoenv_authorized"}
+
 : ${AUTOENV_IN_FILE:='.in'}
 : ${AUTOENV_OUT_FILE:='.out'}
 
 # Normalize file path
 AUTOENV_AUTH_FILE=${AUTOENV_AUTH_FILE:A}
+
+mkdir -p ${AUTOENV_AUTH_FILE:h}
 
 # Create file it if it doesn't exist
 if [[ ! -f $AUTOENV_AUTH_FILE ]]; then
